1c81cc9a9a514df83c557f294a4366ece8b3d556
ahmed-khaled-abdelmaksod <ak4902280@gmail.com>
2025-09-20 13:10:51 +0300

    feat: Integrate MQTT for money tracking in Smart Garden

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import time
 from gpiozero  import LED,DistanceSensor
 import math
 import random
+import paho.mqtt.client as mqtt
+
 #from DIYables_MicroPython_LCD_I2C import LCD_I2C
 #from machine import I2C,Pin
 
 #rtc_sda = Pin(2)
 #rtc_scl = Pin(3)
+broker_ip = '192.168.137.178'
+client = mqtt.Client("Smart-Garden")
+client.connect(broker_ip,1883)
 
 enter_ultra = DistanceSensor(echo=14,trigger=15)
 outer_ultra = DistanceSensor(echo=18,trigger=23)
@@ -54,6 +59,8 @@ while True:
     if entering_distance < 8 and people_inside < people_max_capacity:
        people_inside += 1
        money += child_ticket
+       timestamp = time.time()
+       client.publish("smart-garden/Money",f"{timestamp},{money}")
        print("Money : "+str(money))
 
     elif entering_distance < 15 and people_inside < people_max_capacity:
